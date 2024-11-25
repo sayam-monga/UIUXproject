359c4eddf5964b942f2227cbc9129d8d1666c806
sayam-monga <sayammonga007@gmail.com>
2024-11-25 22:21:42 +0530

    asds

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -4,7 +4,7 @@ const AppBar = () => {
   return (
     <div className="w-screen h-20px flex justify-between items-center px-10 py-4 border-b-2">
       <Link to="/blogs">
-        <div className="text-xl">PenPulse</div>
+        <div className="text-xl">SafeBlog</div>
       </Link>
       <div className="flex flex-row  justify-center  items-center gap-2">
         <div className="mt-2">
